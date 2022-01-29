1b4cffd1ea5bd8eef1545fcfafa63b7553915e43
Babavaraprasad <bapi@itu.dk>
2022-01-29 13:05:46 +0100

    myatm  project changes

---
+++ src/pages/moneytypepage/moneyTypePage.js
@@ -0,0 +1,76 @@
+import ArrowIcon from "../../components/icons/arrowIcon";
+import MoneyBox from "../../components/moneyBox/moneyBox";
+import "./moneytype.css";
+
+function MoneyType({ value, result, backButtonHandler }) {
+	const coinsAndNotes = Object.keys(result).map((v) => Number(v));
+	console.log("result",result);
+	const notes = coinsAndNotes.filter((m) => m >= 50);
+console.log("notes",notes);
+
+	const bigCoins = coinsAndNotes.filter((m) => m < 50 && m >= 2 && m !== 10);
+
+	console.log("bigcoins",bigCoins);
+
+
+	const smallCoins = coinsAndNotes.filter((m) => m === 10 || m === 1);
+
+	console.log("smallcoins",smallCoins);
+	return (
+		<div className="container">
+			<div className="backButton" onClick={backButtonHandler}>
+				<ArrowIcon />
+			</div>
+			<div className="count-container">
+				<div className="heading">
+					<p>Depositing</p>
+				</div>
+				<div className="input">£ {value}</div>
+				{result && (
+					<div className="moneyContainer">
+						{!!notes.length && (
+							<div className="note">
+								{notes.map((n) => (
+									<MoneyBox
+										imageUrl={require("../../assets/500-bill.png")}
+										moneyValue={n}
+										numberOfNotesOrcoins={result[n]}
+										key={n}
+									/>
+								))}
+							</div>
+						)}
+						{!!bigCoins.length && (
+							<div className="bigCoin">
+								{bigCoins.map((n) => (
+									<MoneyBox
+										moneyValue={n}
+										numberOfNotesOrcoins={result[n]}
+										key={n}
+									/>
+								))}
+							</div>
+						)}
+						{!!smallCoins.length && (
+							<div className="smallCoin">
+								{smallCoins.map((n) => (
+									<MoneyBox
+										moneyValue={n}
+										numberOfNotesOrcoins={result[n]}
+										key={n}
+									/>
+								))}
+							</div>
+						)}
+					</div>
+				)}
+				<div className="text">
+					<p>Thank you for using</p>
+					<p>ATM</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default MoneyType;
